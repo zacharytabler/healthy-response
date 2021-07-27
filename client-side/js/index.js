d83c4e58465037c96d63c63f0638a81970e59ca2
jordaneaster <jordaneaster222@icloud.com>
2021-07-27 15:27:29 -0400

    WIP  initial post request on user login returns USERNAME

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,1 +1,44 @@
-console.log('wired');
+import apiActions from "./api-actions/api-actions";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import user_login from "./pages/LoginPage";
+import userWelcome from "./pages/UserProfilePage";
+
+const app = document.querySelector("#app");
+
+buildPage();
+
+function buildPage() {
+  renderUserLogin();
+}
+
+function header() {
+  const headerElement = document.querySelector(".header");
+  headerElement.innerHTML = Header();
+}
+function footer() {
+  const footerElement = document.querySelector(".footer");
+  footerElement.innerHTML = Footer();
+}
+
+function renderUserLogin() {
+  app.innerHTML = userWelcome();
+  app.addEventListener("click", (event) => {
+    if (event.target.classList.contains("create_user")) {
+      const userName =
+        event.target.parentElement.querySelector(".userName").value;
+      console.log(userName);
+      apiActions.postRequest(
+        "http://localhost:8080/create_user",
+        {
+          userName: userName,
+        },
+        () => {
+          console.log("callback firing");
+        }
+      );
+    }
+  });
+}
+
+function renderUsers() {}
