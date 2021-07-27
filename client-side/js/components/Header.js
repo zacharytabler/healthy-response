893780d8904f8159db31637b0adc643f4863fee3
Nick Billhardt <nickbillhardt@ymail.com>
2021-07-26 20:14:53 -0400

    Header and Footer basic layout with some styling

--- client-side/js/components/Header.js
+++ client-side/js/components/Header.js
@@ -2,11 +2,16 @@ export default function Header() {
   return `
         <ul class='nav__list'>
             <li class='nav__list_home'>Home</li>
-            <li class='nav__list_artList'>Art</li>
-            <li class='nav__list_science'>Science</li>
-            <li class='nav__list_history'>History</li>
-            <li class='nav__list_quiz'>Quiz</li>
+            <li class='nav__list_alternatives'>Alternatives</li>
+            <li class='nav__list_consequences'>Consequences</li>
+            <li class='nav__list_coping_mechanisms'>Coping Mechanisms</li>
+            <li class='nav__list_moods'>Moods</li>
+            <li class='nav__list_responses'>Responses</li>
+            <li class='nav__list_results'>Results</li>
+            <li class='nav__list_triggers'>Triggers</li>
+            <li class='nav__list_users'>Users</li>
             <li class='nav__list_aboutUs'>About Us</li>
+            <li class='nav__list_contactUs'>Contact US</li>
         </ul>
     `;
 }
--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,1 +1,19 @@
-console.log('wired');
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
+
+buildPage();
+
+function buildPage(){
+    header();
+    footer();
+}
+
+function header() {
+  const headerElement = document.querySelector(".header");
+  headerElement.innerHTML = Header();
+}
+
+function footer() {
+  const footerElement = document.querySelector(".footer");
+  footerElement.innerHTML = Footer();
+}
