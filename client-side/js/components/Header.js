7ccfc22cd8def1baa6f6abcdc0ad947c3e6ece13
sfafiade <thefaf17@gmail.com>
2021-07-28 14:16:34 -0400

    new page

--- client-side/js/components/Header.js
+++ client-side/js/components/Header.js
@@ -17,6 +17,7 @@ export default function Header() {
             <li class='nav__list_users'>Users</li>
             <li class='nav__list_aboutUs'>About Us</li>
             <li class='nav__list_contactUs'>Contact US</li>
+            <li class='nav__list_appointment'>Appointment</li>
         </ul>
     </nav>
         `;
--- client-side/js/index.js
+++ client-side/js/index.js
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import user_login from "./pages/LoginPage";
 import userWelcome from "./pages/UserProfilePage";
+import Contact from './pages/ContactUS';
 
 const app = document.querySelector("#app");
 
@@ -12,6 +13,7 @@ function buildPage() {
   renderUserLogin();
   header();
   footer();
+  contact();
 }
 
 function header() {
@@ -22,6 +24,14 @@ function footer() {
   const footerElement = document.querySelector(".footer");
   footerElement.innerHTML = Footer();
 }
+function contact() {
+  const homeElem = document.querySelector(".nav__list_contactUs");
+  homeElem.addEventListener("click", () => {
+      const app = document.querySelector("#app");
+      app.innerHTML = Contact();
+  });
+}
+
 
 function renderUserLogin() {
   app.innerHTML = userWelcome();
