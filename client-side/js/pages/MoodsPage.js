dba4632f589916a2d327eb5024a8b6592385914a
zacharytabler <zachary.tabler@gmail.com>
2021-07-30 12:22:28 -0400

    Populating the pages

--- client-side/js/pages/MoodsPage.js
+++ client-side/js/pages/MoodsPage.js
@@ -1,7 +1,23 @@
-export default function MoodsPage(){
+export default function MoodsPage(moods){
     return `
-    
     <h1>Moods Page</h1>
-
+    <div class='moods-constructor'>
+    ${moods.map(mood => {
+        return `
+        <p class='moods-constructor__title'>Mood: ${mood.mood}
+          <input type='hidden' id='moodsId' value='${mood.id}'>
+        </p>
+    ${mood.trigger.map(trigger => {
+        return `
+        <p class='moods-constructor__trigger-title'>Trigger Name: ${trigger.name}
+          <input type='hidden' id='triggerId' value='${trigger.id}'>
+        </p>
+        <p class='moods-constructor__trigger-description'>Trigger Description: ${trigger.description}
+      </p>
+        `
+    }).join('')}
+    `
+    }).join('')}
+    </div>
     `;
 }
--- client-side/js/pages/ResultsPage.js
+++ client-side/js/pages/ResultsPage.js
@@ -1,5 +1,24 @@
-export default function ResultsPage(){
+export default function ResultsPage(results){
     return `
 <h1>Results page</h1>
-    `;
+<div class='results-constructor'>
+${results.map(result => {
+    return `
+    <p class='results-constructor__title'>Result Title: ${result.title}
+      <input type='hidden' id='resultsId' value='${result.id}'>
+    </p>
+    <p class='results-constructor__description'>Result Description: ${result.description}</p>
+${result.alternatives.map(alternatives => {
+    return `
+    <p class='results-constructor__alternatives-title'>Alternative Title: ${alternatives.title}
+      <input type='hidden' id='alternativesId' value='${alternatives.id}'>
+    </p>
+    <p class='results-constructor__alternative-description'>Alternative Description: ${alternatives.description}
+  </p>
+    `
+}).join('')}
+`
+}).join('')}
+</div>
+`;
 }
