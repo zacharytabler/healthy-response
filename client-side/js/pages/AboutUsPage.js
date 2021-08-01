42dd3fe901e9afee78f92115df04e8a89476d88f
zacharytabler <zachary.tabler@gmail.com>
2021-08-01 16:01:39 -0400

    Updated About Us page layout

--- client-side/js/pages/AboutUsPage.js
+++ client-side/js/pages/AboutUsPage.js
@@ -2,16 +2,17 @@ import "../../css/aboutUS.css"
 
 export default function about() {
   return `
-    <div class="container">
-    <div class="header>
-    <h1>Vanguard Health Solutions</h1>
-    <p></p>
+  <div class="container">
+    <div class="header">
+    <h1 class = "aboutTitle">Vanguard Health Solutions</h1>
+    <p class = "valueProp">Vanguard Health Solutions was formed in 2021 to assist individuals with making positive life choices daily. <br> Healthly Response assistant is a fully customizable app designed to help you navigate your unique life circumstances. <br> We want to be your go to resource when you're uncertain how to respond.  </p>
+    <h2 class = "aboutTeam">Meet The Team</h2>
+    </div>
     <div class="team">
-    <h2>Meet The Team</h2>
     <div class="member">
       <img src="https://i.ibb.co/P465vsM/nickpic.jpg" alt="Nick Billhardt Image">
       <h3>Nick Billhardt</h3>
-      <span>nickbillhardt@ymail.com</span>
+      <a href = "mailto: nickbillhardt@ymail.com">nickbillhardt@ymail.com</a>
       <p>Nick Bio.</p>
       <div class="btn">
         <a href="#">LinkedIn</a>
@@ -23,7 +24,7 @@ export default function about() {
     <div class="member">
       <img src="https://i.ibb.co/Hx1GB0L/jordan.jpg" alt="Jordan Easter Image">
       <h3>Jordan Easter</h3>
-      <span>jordaneaster222@icloud.com</span>
+      <a href = "mailto: jordaneaster222@icloud.com">jordaneaster222@icloud.com</a>
       <p>Jordan Bio.</p>
       <div class="btn">
       <a href="#">LinkedIn</a>
@@ -35,7 +36,7 @@ export default function about() {
     <div class="member">
       <img src="https://i.ibb.co/8dxvcfW/stephen.jpg" alt="Stephen Fafiade Image">
       <h3>Stephen Fafiade</h3>
-      <span>thefaf17@gmail.com</span>
+      <a href = "mailto: thefaf17@gmail.com">thefaf17@gmail.com</a>
       <p>Stephen Bio.</p>
       <div class="btn">
       <a href="#">LinkedIn</a>
@@ -47,7 +48,7 @@ export default function about() {
     <div class="member">
     <img src="" alt="Chelsea James Image">
     <h3>Chelsea James</h3>
-    <span>james.chelsea.m@gmail.com</span>
+    <a href = "mailto: james.chelsea.m@gmail.com">james.chelsea.m@gmail.com</a>
     <p>Chelsea Bio.</p>
     <div class="btn">
     <a href="#">LinkedIn</a>
@@ -59,7 +60,7 @@ export default function about() {
   <div class="member">
     <img src="https://i.ibb.co/KrFQZXx/KitSidhu.jpg" alt="Kit Sidhu Image">
     <h3>Kit Sidhu</h3>
-    <span>kitsidhu@gmail.com</span>
+    <a href = "mailto: kitsidhu@gmail.com">kitsidhu@gmail.com</a>
     <p>Kit Bio.</p>
     <div class="btn">
     <a href="#">LinkedIn</a>
@@ -71,7 +72,7 @@ export default function about() {
   <div class="member">
     <img src="https://i.ibb.co/C9JKdFm/Zachary-Tabler.jpg" alt="Zachary Tabler Image">
     <h3>Zachary Tabler</h3>
-    <span>zachary.tabler@gmail.com</span>
+    <a href = "mailto: zachary.tabler@gmail.com">zachary.tabler@gmail.com</a>
     <p>Zachary Bio.</p>
     <div class="btn">
     <a href="#">LinkedIn</a>
--- client-side/js/pages/ResponsesPage.js
+++ client-side/js/pages/ResponsesPage.js
@@ -1,21 +1,24 @@
-export default function ResponsesPage(responses) {
+export default function ResponsesPage() {
   return `
     <h1>Responses page</h1>
-    <div class='responses-constructor'>
-    ${responses.map(response => {
-      return `
-      <p class='responses-constructor__id'><input type='hidden' id='responseId' value='${response.id}'></p>
-      ${response.trigger.map(trigger => {
+   <div class='triggers-constructor'>
+    ${triggers.map(trigger => {
         return `
-        <p class='moods-constructor__trigger-title'>Trigger Name: ${trigger.name}
-          <input type='hidden' id='triggerId' value='${trigger.id}'>
+        <p class='triggers-constructor__name'>Trigger Name: ${trigger.name}
+          <input type='hidden' id='triggersId' value='${trigger.id}'>
         </p>
-        <p class='moods-constructor__trigger-description'>Trigger Description: ${trigger.description}
+        <p class='triggers-constructor__description'> Trigger Description: ${trigger.description}</p>
+    ${trigger.copingMechanism.map(coping => {
+        return `
+        <p class='triggers-constructor__coping-Mechanism-title'>Coping Mechanism Title: ${coping.title}
+          <input type='hidden' id='copingMechanismId' value='${coping.id}'>
+        </p>
+        <p class='triggers-constructor__copingMechanism-description'>Coping Mechanism Description: ${coping.description}
       </p>
-          `
-      }).join('')}  
-  `
-  }).join('')}
-  </div>
+        `
+    }).join('')}
+    `
+    }).join('')}
+    </div>
     `;
 }
