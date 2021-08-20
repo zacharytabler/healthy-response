cff75ed07acb25362311815c098f17217197b832
zacharytabler <zachary.tabler@gmail.com>
2021-08-20 17:35:03 -0400

    Moved reviews out of footer

--- client-side/js/components/Footer.js
+++ client-side/js/components/Footer.js
@@ -3,7 +3,6 @@ export default function Footer() {
   <ul class="footer_nav">
       <li class='footer_list_aboutUs'>About Us</li>
       <li class='footer_list_contactUs'>Contact Us</li>
-      <li class='footer_list_reviews'>Reviews</li>
       <li class='footer_list_legal'>Legal</li>
   </ul>
 <small class="vhs">&copy VanGUARD Health Solutions 2021</small>
